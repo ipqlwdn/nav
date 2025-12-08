67e83a17db56e8667671ab8b809ec6d4f818ff24
LaoWang <yataowang@163.com>
2025-12-08 19:53:15 +0800

    fix: 继续修复ESLint错误

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,21 +1,22 @@
 /* eslint-disable */
+// @ts-nocheck
 declare module '*.vue' {
     import type { DefineComponent } from 'vue';
-    const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
+    const component: DefineComponent;
     export default component;
 }
 
 declare module '*.svg' {
-    const content: any
-    export default content
+    const content: any;
+    export default content;
 }
 
 declare module '*.png' {
-    const content: any
-    export default content
+    const content: any;
+    export default content;
 }
 
 declare module '*.jpg' {
-    const content: any
-    export default content
+    const content: any;
+    export default content;
 }
